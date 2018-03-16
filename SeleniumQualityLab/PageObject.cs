91a29d5d41acf00b5021384a0c6461152246c120
TimHastelloy <31857607+TimHastelloy@users.noreply.github.com>
2018-03-16 03:37:02 +0300

    Test
    PAge Object for mail.ru

---
+++ SeleniumQualityLab/PageObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+
+namespace SeleniumQualityLab
+{
+    class PageObject
+    {
+        private IWebDriver driver;
+        By login_element;
+        By password_element;
+        By enter_button;
+        By send_button;
+        By address_to;
+        By table;
+        public PageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void MailLoginPage()
+        {
+            string login = ""; /*Input your login*/
+            string password = "";/*Input your password*/
+            driver.Navigate().GoToUrl("https://mail.ru/");
+            Assert.IsTrue(driver.Url=="https://mail.ru/");
+            login_element = By.Name("login");
+            Assert.NotNull(login_element);
+            driver.FindElement(login_element).SendKeys(login); 
+            password_element= By.Name("password");
+            Assert.NotNull(password_element);
+            driver.FindElement(password_element).SendKeys(password);
+            enter_button = By.XPath("//input[@value='Войти']");
+            Assert.NotNull(enter_button);
+            driver.FindElement(enter_button).Click();
+            Thread.Sleep(5000);
+           
+        }
+        public void SendMessage()
+        {
+            send_button = By.XPath("//span[text()='Написать письмо']");
+            Assert.NotNull(send_button);
+            driver.FindElement(send_button).Click();
+            address_to = By.XPath("//textarea[@data-original-name='To']");
+            Assert.NotNull(address_to);
+            driver.FindElement(address_to).SendKeys("test@gmail.com");
+        }
+    }
+}
---
+++ SeleniumQualityLab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumQualityLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            PageObject po = new PageObject(driver);
+            po.MailLoginPage();
+            po.SendMessage();
+        }
+    }
+}
